4d8e0c87a384a4124e4cb6a6667177e905a21d4e
Ankur <ankurbanerji3@gmail.com>
2023-01-26 16:27:23 -0500

    Update tests

--- README.md
+++ README.md
@@ -1,22 +1,2 @@
-# Homeworks
-CSC 591-021 Group-8 Homework Repo
-
-[![DOI](https://zenodo.org/badge/590194021.svg)](https://zenodo.org/badge/latestdoi/590194021)
-[![Collaborators](https://img.shields.io/badge/Collaborators-3-purple.svg?style=flat)](https://github.com/SN-18/ASE-group-8/graphs/contributors)
-[![Language](https://img.shields.io/badge/Language-Python-orange.svg?style=flat)](https://github.com/SN-18/ASE-group-8/search?l=python)
-[![License](https://img.shields.io/badge/License-MIT-blue.svg?style=flat)](https://github.com/SN-18/ASE-group-8/blob/main/LICENSE.md)
-[![Tests](https://github.com/SN-18/ASE-group-8/actions/workflows/tests.yml/badge.svg)](https://github.com/SN-18/ASE-group-8/actions/workflows/tests.yml)
-
-# About
-Homework repository for **CSC 591 021 Group-8**<br/>
-- HW1 - Conversion of <a href="https://github.com/timm/tested/blob/main/src/script.lua">script.lua</a> to Python, including the test cases. Automated workflows using GitHub Actions.
-
-# Steps to run
-- HW1:
-  1. Install Python 3.10.6
-  2. cd into src folder of homework and run ```python main.py -g all```. Use ```--help``` option for a list of all options and change arguments likewise.
-
-# Team Members
- - Saurabh Nanda (snanda2@ncsu.edu)
- - Luke Jenquin  (lgjenqui@ncsu.edu)
- - Ankur Banerji (abaner24@ncsu.edu)
+# testing_ase_g8
+# testing_ase_g8
---
+++ src/HW2/tests.py
@@ -0,0 +1,65 @@
+the, help = {}, '''   
+script.lua : an example script with help text and a test suite
+(c)2022, Tim Menzies <timm@ieee.org>, BSD-2 
+USAGE:   script.lua  [OPTIONS] [-g ACTION]
+OPTIONS:
+  -d  --dump  on crash, dump stack = false
+  -g  --go    start-up action      = data
+  -h  --help  show help            = false
+  -s  --seed  random number seed   = 937162211
+ACTIONS:
+'''
+from tables import *
+from symbols import *
+from data_loader import *
+from globals import the as the
+
+
+egs = {}
+
+
+def eg(key, string, fun, help):
+    egs[key] = fun
+    help = help + " -g " + str(key) + "\t" + str(string) + "\n"
+
+
+def the_test_1():
+    print(the)
+
+
+
+def sym_test_3():
+    sym = SYM(at=0,txt="")
+    for x in ["a", "a", "a", "a", "b", "b", "c"]:
+        sym.add(x)
+    return "a" == sym.mid() and 1.379 == round_n(sym.div())
+
+
+def num_test_4():
+    num = NUM(at=0,txt="")
+    for x in [1, 1, 1, 1, 2, 2, 3]:
+        num.add(x)
+    return 11 / 7 == num.mid() and 0.787 == round_n(num.div())
+
+
+def fun(t,n=0):
+    n = n + len(t)
+
+
+def csv_5():
+    loaded_table=csv(the['file'], fun)
+    return len(loaded_table)==8*399
+
+def data_6():
+    data=DATA(the['file'])
+    return len(data.rows)==398 and \
+    data.cols.x[1].at==1 and \
+    data.cols.y[0].w == -1 and \
+    len(data.cols.x)==4
+
+def stats_7():
+    data = DATA(the['file'])
+    d = {'y': data.cols.y, 'x': data.cols.x}
+    for k, cols in d.items():
+        print(k + 'mid'+ str(data.stats('mid', cols, 2)))
+        print(''+ 'div' +str(data.stats('div', cols, 2)))
