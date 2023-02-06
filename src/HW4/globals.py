899fdcff9884b6f37f3c29cf9b7e85e1987c27e2
Luke Jenquin <lgjenqui@ncsu.edu>
2023-02-05 22:21:00 -0500

    Added globals, repgrid1 data, and settings.py

---
+++ src/HW4/globals.py
@@ -0,0 +1,15 @@
+the, help = {},\
+'''   
+script.lua : an example script with help text and a test suite
+(c)2022, Tim Menzies <timm@ieee.org>, BSD-2 
+USAGE:   script.lua  [OPTIONS] [-g ACTION]
+OPTIONS:
+  -d  --dump   on crash, dump stack   = false
+  -f  --file   name of file           = ./repgrid1.csv
+  -g  --go     start-up action        = data
+  -h  --help   show help              = false
+  -p  --p      distance coefficient   = 2
+  -s  --seed   random number seed     = 937162211
+ACTIONS:
+'''
+seed = 937162211
