b9f027fd1d8845a6e3f02be289da0db00f06d01b
Saurabh Nanda <83748468+SN-18@users.noreply.github.com>
2023-03-30 14:35:15 -0400

    Add functions, remove placeholders

--- src/HW7/functions.py
+++ src/HW7/functions.py
@@ -5,27 +5,25 @@ import os
 import re
 import math
 from globals import the as the
-import globals
+from globals import the
+from globals import *
 from tables import *
 from sys import maxsize
-
+import types
 b4 = {}
+
 
 #miscelleneous functions
 def rand_intpy(lo, hi):
     globals.seed, val = rand_fltpy(lo, hi, globals.seed)
-
     return math.floor(0.5 + val)
 
 def rint(lo, hi, seed):
     seed, val = rand_fltpy(lo, hi, seed)
-
     return seed, math.floor(0.5 + val)
 
 def rand_fltpy(lo, hi, seed):
-
     lo, hi = lo or 0, hi or 1
-
     seed = (16807 * seed) % 2147483647
     return seed, lo + (hi - lo) * seed / 2147483647
 
@@ -315,6 +313,12 @@ def extend(range, n, s):
     range['lo'] = min(n, range['lo'])
     range['hi'] = max(n, range['hi'])
     range['y'].add(s)
+
+# def merges(lo,hi):
+#     out = RX([], rxs[i]['name'])
+#     for k in range(i, j+1):
+#         out=merge(out, rxs[j])
+#     return out
 
 def mergeAny(ranges0):
     def noGaps(t):
@@ -641,26 +645,158 @@ def on(x):
     return fun
 
 def gaussian(mu,sd):
-    pass
+    mu,sd=mu or 0, sd or 1
+    sq,pi, log, cos, r=math.sqrt, math.pi, math.log, math.cos,random.random
+    return mu + sd*sq(-2*log(r()))*cos(2*pi*r())
 
-#dummy scottKnot
-def scottKnot():
-    pass
 
-def bootstrap():
-    pass
+def scottKnot(rxs):
+    def merges(i,j):
+        out=RX([],rxs[i]['name'])
+        for k in range(i,j+1):
+            out=merge(out,rxs[j])
+        return out
 
-def tiles():
-    pass
 
-def mid():
-    pass
-def RX():
-    pass
-def samples():
-    pass
+    def same(lo,cut,hi):
+        merges=types.FunctionType(scottKnot.__code__.co_consts[1],{})
+        l=merges(lo,cut)
+        r=merges(cut + 1,hi)
+        return cliffsDelta(l['has'],r['has']) and bootstrap(l['has'],r['has'], NUM)
 
-def delta():
-    pass
+    def recurse(lo, hi, rank):
+        merges = types.FunctionType(scottKnot.__code__.co_consts[1], {})
+        b4 = merges(lo, hi)
+        best = 0
+        cut = None
+        for j in range(lo, hi + 1):
+            if j < hi:
+                l = merges(lo, j)
+                r = merges(j + 1, hi)
+                now = (l['n'] * (mid(l) - mid(b4)) ** 2 + r['n'] * (mid(r) - mid(b4)) ** 2) / (l['n'] + r['n'])
+                if now > best:
+                    cohen = the.cohen
+                    if abs(mid(l) - mid(r)) >= cohen:
+                        cut, best = j, now
+
+        if cut is not None and not same(lo, cut, hi):
+            rank = recurse(lo, cut, rank) + 1
+            rank = recurse(cut + 1, hi, rank)
+        else:
+            for i in range(lo, hi + 1):
+                rxs[i]['rank'] = rank
+        return rank
+
+    rxs = rxs_sort(rxs)
+    cohen = div(merges(0, len(rxs) - 1)) * the['cohen']
+    recurse(0, len(rxs) - 1, 1)
+    return rxs
+
+def bootstrap(y0,z0):
+    x, y, z, yhat, zhat= NUM(), NUM(), NUM(), [], []
+    for y1 in y0:
+        x.add(y1)
+        y.add(y1)
+    for z1 in z0:
+        x.add(z1)
+        z.add(z1)
+    xmu, ymu, zmu = x.mu, y.mu, z.mu
+    for y1 in y0:
+        yhat.append(y1 - ymu + xmu)
+    for z1 in z0:
+        zhat.append(z1 - zmu+ xmu)
+
+    tobs=delta(y,z)
+    n=0
+    for iter in range(1,the['bootstrap']+ 1):
+        i=NUM()
+        other=NUM()
+        for y in samples(yhat).values():
+            i.add(y)
+        for z in samples(zhat).values():
+            other.add(z)
+        if delta(i, other)> tobs:
+            n = n + 1
+    return n/the['bootstrap']>=the['conf']
+
+
+def tiles(rxs):
+    huge = float('inf')
+    lo,hi = huge, float('-inf')
+    const_one = 1
+
+    for rx in rxs:
+        lo,hi = min(lo,rx['has'][0]), max(hi, rx['has'][len(rx['has'])-1])
+    for rx in rxs:
+        t,u = rx['has'],[]
+        def of(x, most):
+            return int(max(0, min(most, x)))
+        def at(x):
+            return t[of(len(t)*x//const_one, len(t))]
+        def pos(x):
+            return math.floor(of(the['width']*(x - lo)/(hi - lo + 1E-32)//const_one, the['width']))
+
+        for i in range(the['width'] + 1):
+            u.append(" ")
+
+        a,b,c,d,e = at(0.1),at(0.3),at(0.5),at(0.7),at(0.9)
+        A,B,C,D,E = pos(a), pos(b), pos(c), pos(d), pos(e)
+
+        for i in range(A, B+1):
+            u[i]="-"
+        u[the['width']//2]= "|"
+        u[C]= "*"
+        x= []
+
+        for i in [a,b,c,d,e]:
+            x.append(the['Fmt'].format(i))
+        rx['show'] = ''.join(u) + str(x)
+    return rxs
+
+
+
+def mid(t):
+    t=t['has'] if t['has'] else t
+    n=(len(t)-1)//2
+    return (t[n] + t[n+1])/2 if len(t)%2==0 else t[n+1]
+
+def RX(t,s):
+    t = sorted(t)
+    return {'name': s or "", 'rank':0, 'n':len(t),\
+            'show':"",'has':t}
+
+def div(t):
+    if t['has']:
+        t = t['has']
+    const_nine = 9
+    const_one = 1
+    return (t[len(t) * const_nine // 10] - t[len(t) * const_one // 10]) / 2.56
+
+
+def RX(t,s):
+    t = sorted(t)
+    return {'name':s or "",'rank':0, 'n':len(t), 'show':"", 'has':t}
+
+def samples(t,n):
+    u=dict()
+    for i in range(1, (n or len(t) + 1)):
+        u[i] =t[random.randint(0,len(t) -1)]
+    return u
+
+def delta(i,other):
+    e, y, z= 1E-32, i, other
+    return abs(y.mu - z.mu)/ ((e + y.sd**2/y.n + z.sd**2/z.n)**0.5)
+
+
+
+def rxs_sort(rxs):
+    for i,x in enumerate(rxs):
+        for j,y in enumerate(rxs):
+            if mid(x) < mid(y):
+                rxs[j],rxs[i] = rxs[i],rxs[j]
+    return rxs
+
+
+
 
 
